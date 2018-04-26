e007a98838dcdee2f491131e035f755340c3b175
tataiermail <sumanchakraborty.kol@gmail.com>
2018-04-25 22:19:29 -0400

    added XML generator code from Sindhu

--- MARS/src/main/java/cts/mars/compUtils.java
+++ MARS/src/main/java/cts/mars/compUtils.java
@@ -33,6 +33,20 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
 //import org.sikuli.script.*;
 
 
@@ -431,7 +445,171 @@ public class compUtils {
         }
         }
 	
-	
+	static void batchXmlCreator (String tagsFile, String outputFile) {
+		
+		String fileName = tagsFile;
+		String batchFile = outputFile;	
+	try {
+		FileInputStream excelFile = new FileInputStream(new File(fileName));
+        Workbook workbook = new XSSFWorkbook(excelFile);
+        org.apache.poi.ss.usermodel.Sheet datatypeSheet =  workbook.getSheetAt(0);
+		System.out.println("sheet:" + datatypeSheet.getSheetName());
+        Iterator<Row> iterator = datatypeSheet.iterator();
+		int rowIterator = 0;
+        while (iterator.hasNext()) {
+        	if(rowIterator ==0)
+        	{
+        		iterator.next();
+        		rowIterator++;
+        	}
+        	String FirstName = null;
+    		String LastName = null;
+    		String DOB = null;
+    		String Gender = null;
+    		String MaritalStatus = null;
+    		String AddressLineOne = null;
+    		String City = null;
+    		String State = null;
+    		String Zip = null;
+    		String Country = null;
+    		String phoneNumber = null;
+    		String Email = null;
+    		String SSN = null;
+    		String DL = null;
+    		String DLissueDate = null;
+    		String ClientId = null;
+    		String AccountNumber = null;
+    		
+    		
+            Row currentRow = iterator.next();
+            Iterator<Cell> cellIterator = currentRow.iterator();
+            int i=0;
+            while (cellIterator.hasNext()) {
+
+            	Cell currentCell = cellIterator.next();
+            	
+                //getCellTypeEnum shown as deprecated for version 3.15
+                //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+            	
+                       	
+            	 String strCellValue = null;
+                 if (currentCell != null) {
+                     switch (currentCell.getCellType()) {
+                     case Cell.CELL_TYPE_STRING:
+                         strCellValue = currentCell.toString();
+                         break;
+                     case Cell.CELL_TYPE_NUMERIC:
+                         if (DateUtil.isCellDateFormatted(currentCell)) {
+                             SimpleDateFormat dateFormat = new SimpleDateFormat(
+                                     "dd/MM/yyyy");
+                             strCellValue = dateFormat.format(currentCell.getDateCellValue());
+                         } else {
+                             Double value = currentCell.getNumericCellValue();
+                             Long longValue = value.longValue();
+                             strCellValue = new String(longValue.toString());
+                         }
+                         break;
+                     case Cell.CELL_TYPE_BOOLEAN:
+                         strCellValue = new String(new Boolean(
+                        		 currentCell.getBooleanCellValue()).toString());
+                         break;
+                     case Cell.CELL_TYPE_BLANK:
+                         strCellValue = "";
+                         break;
+                     }
+                    //System.out.println(strCellValue); 
+                    switch(i)
+                    {
+                    case 0:
+                    	FirstName = strCellValue;
+                    	break;
+                    case 1:
+                    LastName = strCellValue;
+                	break;
+                    case 2: 
+                    	DOB = strCellValue;
+                    	break;
+                    case 3: 
+                    	Gender = strCellValue;
+                    	break;
+                    case 4: 
+                    	MaritalStatus = strCellValue;
+                    	break;
+                    case 5: 
+                    	AddressLineOne = strCellValue;
+                    	break;
+                    case 6: 
+                    	City = strCellValue;
+                    	break;
+                    	
+                    case 7: 
+                    	State = strCellValue;
+                    	break;
+                    	
+                    case 8: 
+                    	Zip = strCellValue;
+                    	break;
+                    case 9: 
+                    	Country = strCellValue;
+                    	break;
+                    case 10: 
+                    	phoneNumber = strCellValue;
+                    	break;
+                    case 11: 
+                    	Email = strCellValue;
+                    	break;
+                    case 12: 
+                    	SSN = strCellValue;
+                    	break;
+                    case 13: 
+                    	DL = strCellValue;
+                    	break;
+                    case 14: 
+                    	DLissueDate = strCellValue;
+                    	break;
+                    case 15: 
+                    	ClientId = strCellValue;
+                    	break;
+                    case 16: 
+                    	AccountNumber = strCellValue;
+                    	break;
+                    }
+                    
+                    i++;
+                    if(i==17)
+                    {
+                   System.out.println("FN:"+FirstName+"LN:"+LastName+"DOB:"+DOB+"Gender:"+Gender+"MaritalStatus:"+MaritalStatus+"A1:"+AddressLineOne+"City:"+City+"State:"+State+"Zip:"+Zip+"Country:"+Country+"phone:"+phoneNumber+"email:"+Email+"SSN:"+SSN+"DL:"+DL+"DLID:"+DLissueDate+"ClientID:"+ClientId+"AccountNumber:"+AccountNumber); 
+                   
+                   DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+               	Date date = new Date();
+               	System.out.println("Date Executed: " + dateFormat.format(date));
+                
+               	String inputXML = null;
+               	inputXML = "<TCRMService xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.ibm.com/mdm/schema\" xsi:schemaLocation=\"http://www.ibm.com/mdm/schema MDMDomains.xsd\"> <RequestControl> <requestID>180111101838389002</requestID> <DWLControl> <requesterName>gwpc</requesterName> <requesterLanguage>100</requesterLanguage> <requesterLocale>en</requesterLocale> <clientTransactionName>"+ClientId+"</clientTransactionName> <requestID>180111101838389002</requestID> </DWLControl> </RequestControl> <TCRMTx> <TCRMTxType>MaintainFIGParty</TCRMTxType> <TCRMTxObject>FIGCustomerBObj</TCRMTxObject> <TCRMObject> <FIGCustomerBObj> <ComponentID>11000089</ComponentID> <TCRMPersonBObj> <PartyType>P</PartyType> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> <BirthDate>"+DOB+"</BirthDate> <GenderType>"+Gender+"</GenderType> <MaritalStatusType>"+MaritalStatus+"</MaritalStatusType> <PartyActiveIndicator>Y</PartyActiveIndicator> <TCRMPartyAddressBObj> <AddressUsageType>1</AddressUsageType> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> <TCRMAddressBObj> <ResidenceType>1000011</ResidenceType> <AddressLineOne>"+AddressLineOne+"</AddressLineOne> <City>"+City+"</City> <ZipPostalCode>"+Zip+"</ZipPostalCode> <ProvinceStateType>"+State+"</ProvinceStateType> <CountryType>"+Country+"</CountryType> </TCRMAddressBObj> </TCRMPartyAddressBObj> <TCRMPartyContactMethodBObj> <ContactMethodUsageType>5</ContactMethodUsageType> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> <TCRMContactMethodBObj> <ReferenceNumber>"+phoneNumber+"</ReferenceNumber> <ContactMethodType>1</ContactMethodType> </TCRMContactMethodBObj> </TCRMPartyContactMethodBObj> <TCRMPartyContactMethodBObj> <ContactMethodUsageType>7</ContactMethodUsageType> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> <TCRMContactMethodBObj> <ReferenceNumber>"+Email+"</ReferenceNumber> <ContactMethodType>2</ContactMethodType> </TCRMContactMethodBObj> </TCRMPartyContactMethodBObj> <TCRMPartyIdentificationBObj> <IdentificationType>1</IdentificationType> <IdentificationNumber>"+SSN+"</IdentificationNumber> <IdentificationStatusType>1000014</IdentificationStatusType> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> </TCRMPartyIdentificationBObj> <TCRMPartyIdentificationBObj> <IdentificationType>3</IdentificationType> <IdentificationNumber>"+DL+"</IdentificationNumber> <IdentificationStatusType>1000014</IdentificationStatusType> <StartDate>"+DLissueDate+"</StartDate> <IdentificationIssueLocation>FL</IdentificationIssueLocation> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> </TCRMPartyIdentificationBObj> <TCRMPartyIdentificationBObj> <IdentificationType>1000001</IdentificationType> <IdentificationNumber>679182</IdentificationNumber> </TCRMPartyIdentificationBObj> <TCRMAdminContEquivBObj> <AdminPartyId>"+ClientId+"</AdminPartyId> <AdminSystemType>1000008</AdminSystemType> </TCRMAdminContEquivBObj> <TCRMPersonNameBObj> <NameUsageType>1</NameUsageType> <GivenNameOne>"+FirstName+"</GivenNameOne> <LastName>"+LastName+"</LastName> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <SourceIdentifierType>1000014</SourceIdentifierType> </TCRMPersonNameBObj> <TCRMPartyValueBObj> <ComponentID>1036</ComponentID> <PartyValueType>1000002</PartyValueType> <SourceIdentType>1000014</SourceIdentType> <Attribute0Type>1000007</Attribute0Type> <Attribute0String>"+date+"</Attribute0String> <Attribute1Type>1000002</Attribute1Type> <Attribute1String>Engineer</Attribute1String> </TCRMPartyValueBObj> </TCRMPersonBObj> <FIGAccountBObj> <ComponentID>11000091</ComponentID> <AccountNum>"+AccountNumber+"</AccountNum> <AccountType>1000003</AccountType> <LastVerifiedDate>"+dateFormat.format(date)+"</LastVerifiedDate> <AccountRoleType>3</AccountRoleType> </FIGAccountBObj> </FIGCustomerBObj> </TCRMObject> </TCRMTx> </TCRMService>";
+               	
+               	FileWriter fw = new FileWriter(batchFile, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+               	
+              
+                  // BufferedWriter bw = new BufferedWriter(new FileWriter(batchFile,true)); 
+           	bw.write(inputXML);
+           	System.out.println("XML Written to Output File");
+           	bw.newLine();
+           	bw.close();
+                    }
+                   
+                 }
+            }	
+        }
+
+	} catch (FileNotFoundException e) {
+        e.printStackTrace();
+        System.out.println("File Missing");
+    } catch (IOException e) {
+        e.printStackTrace();
+        System.out.println("IO Exception Happened");
+    }
+	}
 
 	public static void main(String[] args) {
 		
@@ -444,6 +622,11 @@ public class compUtils {
 	    	String sqlFile1 = args[1];
 			String sqlFile2 = args[2];
 			sqlVsSql(sqlFile1, sqlFile2);
+	    }
+	    else if (args[0].equals("batchXmlCreator")) {
+	    	String tagsFile = args[1];
+			String outputFile = args[2];
+			batchXmlCreator(tagsFile, outputFile);
 	    }
 	    else if(args[0].equals("xls2CsvConv")) {  //xls2CsvConv C:\Users\tatai\Dropbox\Career\Skills\Jmeter_Frameworks\bin\MARS_Test_Data_Input.xlsx C:\Users\tatai\Dropbox\Career\Skills\Jmeter_Frameworks\bin\Test_Data_Input.csv SqlVsMql ;
 	    	String xls= null;
